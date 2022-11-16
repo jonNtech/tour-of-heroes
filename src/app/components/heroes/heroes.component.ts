05874a52c00f41716f66caecc94f616051159957
Jon Pena <iamjonntech@gmail.com>
2022-11-15 21:31:05 -0700

    tour of heroes

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../hero';
+import { HeroService } from '../../services/hero.service';
+import { MessageService } from '../../services/message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  //selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  // this is no longer needed nowe that we have different views and routes
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroComponent: Selected hero id=${hero.id}`);
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+}
